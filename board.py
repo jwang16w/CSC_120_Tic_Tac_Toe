24a268366bebd265f83b6f00599e2d88d5c907d5
Jing Wang <jwang16@waketech.edu>
2022-07-20 10:43:10 -0400

    Board Printing functionality added

---
+++ board.py
@@ -0,0 +1,22 @@
+board = [['-', '-', '-'],['-', '-', '-'],['-', '-', '-']] 
+# this list contains 3 lists, each of 3 elements, each element being a single character
+
+def print_board():
+   print("Printing board")
+   for myList in board:
+      print(myList)
+   pass
+
+def check_mark():
+   pass
+
+def place_mark():
+   pass
+
+def check_win():
+   pass
+
+def main():
+   print_board()
+
+main()
